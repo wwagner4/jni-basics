c6a405a15e45be9bfdbfc636d3596ad0d86c9340
wwagner4 <wwagner4@gmail.com>
2016-12-03 10:53:58 +0100

    can acces int variable

--- HelloJni.cpp
+++ HelloJni.cpp
@@ -33,9 +33,14 @@ JNIEXPORT void JNICALL Java_HelloJni_array(JNIEnv* env, jobject obj, jintArray a
 
 JNIEXPORT void JNICALL Java_HelloJni_obj(JNIEnv* env, jobject, jobject obj) {
 	printf("C obj %p\n", obj);
+	
 	jclass clazz = env->GetObjectClass(obj);
 	jfieldID fid1 = env->GetFieldID(clazz, "i", "I");
 	printf("C obj intFieldId %d\n", fid1);
+	
+	jint i = env->GetIntField(obj, fid1);
+  printf("C, i %d\n", i);
+
 }
 
 
