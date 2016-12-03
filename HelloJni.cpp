35581f56e9eebbc6b6a817becd260a38e8287957
wwagner4 <wwagner4@gmail.com>
2016-12-03 09:25:27 +0100

    passing int argument OK

--- HelloJni.cpp
+++ HelloJni.cpp
@@ -4,12 +4,17 @@
 
 JNIEXPORT void JNICALL Java_HelloJni_sayHello(JNIEnv *, jobject){
 	printf("C Hello from cpp\n");
-	return;
 }
 
 JNIEXPORT jint JNICALL Java_HelloJni_r (JNIEnv *, jobject) {
 	printf("C r\n");
 	return 111;	
 }	
+
+JNIEXPORT void JNICALL Java_HelloJni_a(JNIEnv *, jobject, jint a) {
+	printf("C a %d\n", a);
+}
+
+
 
 
--- HelloJni.java
+++ HelloJni.java
@@ -1,24 +1,33 @@
 public class HelloJni {
 
   static {
-  	System.out.println("libpath:" + System.getProperty("java.library.path"));
     System.loadLibrary("HelloJni");
   }
   
   public static void main(String... args) {
   	HelloJni h = new HelloJni();
+  	System.out.println("-------------------------------------");
   	System.out.println("J sayHello ->");
     h.sayHello();
   	System.out.println("J sayHello <-");
+  	System.out.println("-------------------------------------");
 
-  	System.out.println("J sayHello ->");
+  	System.out.println("J r ->");
     int r = h.r();
-  	System.out.println("J sayHello <- " + r);
+  	System.out.println("J r <- " + r);
+  	System.out.println("-------------------------------------");
+
+  	System.out.println("J a -> 555");
+    h.a(555);
+  	System.out.println("J a <- ");
+  	System.out.println("-------------------------------------");
   }
 
   public native void sayHello();
 
   public native int r();
+  
+  public native void a(int a);
   
   
 }
