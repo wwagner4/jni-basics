ded0462d57264aea186b9875290a478a3e99340b
wwagner4 <wwagner4@gmail.com>
2016-12-03 10:26:44 +0100

    Exchanged NULL by 0

--- HelloJni.cpp
+++ HelloJni.cpp
@@ -17,12 +17,12 @@ JNIEXPORT void JNICALL Java_HelloJni_a(JNIEnv* env, jobject obj, jint a) {
 }
 
 JNIEXPORT void JNICALL Java_HelloJni_s(JNIEnv* env, jobject, jstring s) {
-	const char* s1 = env->GetStringUTFChars(s, NULL);
+	const char* s1 = env->GetStringUTFChars(s, 0);
 	printf("C s1 %s\n", s1);
 }
 
 JNIEXPORT void JNICALL Java_HelloJni_array(JNIEnv* env, jobject obj, jintArray a) {
-	jint* ca = env->GetIntArrayElements(a, NULL);
+	jint* ca = env->GetIntArrayElements(a, 0);
   jsize l = env->GetArrayLength(a);
 	printf("C l %d\n", l);
   for (int i=0; i<l; i++) {
