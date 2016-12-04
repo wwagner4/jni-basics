351e00292416822dbee31b039899cdd91cdd3ce7
wwagner4 <wwagner4@gmail.com>
2016-12-04 13:09:35 +0100

    Refactoring

--- HelloJni.cpp
+++ HelloJni.cpp
@@ -3,44 +3,8 @@
 #include <string.h>
 #include "HelloJni.h"
 
-JNIEXPORT void JNICALL Java_HelloJni_sayHello(JNIEnv* env, jobject obj){
-	printf("C Hello from cpp\n");
-}
-
-JNIEXPORT jint JNICALL Java_HelloJni_r (JNIEnv* env, jobject obj) {
-	printf("C r\n");
-	return 111;	
-}	
-
-JNIEXPORT void JNICALL Java_HelloJni_a(JNIEnv* env, jobject obj, jint a) {
-	printf("C a %d\n", a);
-}
-
-JNIEXPORT void JNICALL Java_HelloJni_s(JNIEnv* env, jobject, jstring s) {
-	if (s != NULL) {
-		const char* s1 = env->GetStringUTFChars(s, 0);
-		printf("C s1 %s\n", s1);
-		env->ReleaseStringUTFChars(s, s1);
-	} else {
-		printf("C s1 null\n");
-	}
-}
-
-JNIEXPORT void JNICALL Java_HelloJni_array(JNIEnv* env, jobject obj, jintArray a) {
-	if (a != NULL) {
-		jint* ca = env->GetIntArrayElements(a, 0);
-	  jsize l = env->GetArrayLength(a);
-		printf("C l %d\n", l);
-	  for (int i=0; i<l; i++) {
-			printf("C a[%d] %d\n", i, ca[i]);
-	  }
-	} else {
-		printf("C a null\n");
-	}
-}
-
-// Access the fields of an object
-JNIEXPORT void JNICALL Java_HelloJni_obj(JNIEnv* env, jobject, jobject obj) {
+// Utillity function to output the java class 'C'
+void showC(JNIEnv* env, jobject obj) {
 	jclass clazz = env->GetObjectClass(obj);
 	
 	// access integer value
@@ -82,3 +46,45 @@ JNIEXPORT void JNICALL Java_HelloJni_obj(JNIEnv* env, jobject, jobject obj) {
 	  }
 	}
 }
+
+JNIEXPORT void JNICALL Java_HelloJni_sayHello(JNIEnv* env, jobject obj){
+	printf("C Hello from cpp\n");
+}
+
+JNIEXPORT jint JNICALL Java_HelloJni_r (JNIEnv* env, jobject obj) {
+	printf("C r\n");
+	return 111;	
+}	
+
+JNIEXPORT void JNICALL Java_HelloJni_a(JNIEnv* env, jobject obj, jint a) {
+	printf("C a %d\n", a);
+}
+
+JNIEXPORT void JNICALL Java_HelloJni_s(JNIEnv* env, jobject, jstring s) {
+	if (s != NULL) {
+		const char* s1 = env->GetStringUTFChars(s, 0);
+		printf("C s1 %s\n", s1);
+		env->ReleaseStringUTFChars(s, s1);
+	} else {
+		printf("C s1 null\n");
+	}
+}
+
+JNIEXPORT void JNICALL Java_HelloJni_array(JNIEnv* env, jobject obj, jintArray a) {
+	if (a != NULL) {
+		jint* ca = env->GetIntArrayElements(a, 0);
+	  jsize l = env->GetArrayLength(a);
+		printf("C l %d\n", l);
+	  for (int i=0; i<l; i++) {
+			printf("C a[%d] %d\n", i, ca[i]);
+	  }
+	} else {
+		printf("C a null\n");
+	}
+}
+
+// Access the fields of an object
+JNIEXPORT void JNICALL Java_HelloJni_obj(JNIEnv* env, jobject, jobject obj) {
+	showC(env, obj);
+}
+
